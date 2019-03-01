9590844ceb9f65e1d3bef9c3fefe9a7528626ecb
Justin Hathaway <hathaway.justin@gmail.com>
2019-03-01 16:57:50 -0700

    adds buy animation and does not allow user to initiate more than one buy sequence per item at a time

--- src/components/click-item/click-item.js
+++ src/components/click-item/click-item.js
@@ -10,29 +10,50 @@ class ClickItem extends Component {
     // get the correct state from db and only reference props. let app own the state
     super(props);
 
+    // Ideally this would be altered before storing based on current level, etc.
     this.state = {
       clickValue: props.item.initialClickValue,
       upgradePrice: props.item.initialUpgradePrice,
       buyTime: props.item.initialBuyTime,
+      name: props.item.name,
+      isBuying: false
     };
 
     this.buy = this.buy.bind(this);
     this.upgrade = this.upgrade.bind(this);
     this.updateInternalState = this.updateInternalState.bind(this);
+    this.updateStylesheetAnimation = this.updateStylesheetAnimation.bind(this);
 
     this.updateInternalState();
+    this.updateStylesheetAnimation();
   }
 
   buy() {
-    this.props.addMoney(this.props.item.initialClickValue)
+    if (this.state.isBuying) return false;
+    this.state.isBuying = true;
+    const selector = `.click-item__buy-button__progress.${this.state.name}`;
+    document.querySelector(selector).classList.add('active');
+
+    setTimeout(() => {
+      this.props.addMoney(this.props.item.initialClickValue);
+      this.state.isBuying = false;
+      console.log(document.querySelector(selector).classList.remove('active'));
+    }, this.state.buyTime * 1000)
   }
 
   upgrade() {
     // stubby
   }
 
+  updateStylesheetAnimation() {
+    const selector = `.click-item__buy-button__progress.${this.state.name}.active`;
+    const rule = `transition: all ${this.state.buyTime}s linear;`;
+    console.log((`${selector} {${rule}}`));
+    document.styleSheets[0].insertRule(`${selector} {${rule}}`);
+  }
+
   updateInternalState() {
-    console.log(this.state);
+    //this will set the state based on the props passed from app (which come from db)
     const clickValue = 0;
     const upgradePrice = 0;
     const buyTime = 0;
@@ -41,10 +62,13 @@ class ClickItem extends Component {
   render() {
     return (
       <div className="click-item">
-        <div className="click-item__buy-button noselect" onClick={this.buy}>Buy</div>
+        <div className="click-item__buy-button noselect" onClick={this.buy}>
+          <span className="click-item__buy-button_text">Buy</span>
+          <div className={"click-item__buy-button__progress " + this.state.name + " noselect"}></div>
+        </div>
         <div className="click-item__info-container">
           <div className="click-item__image">
-            <img src={Images[this.props.item.imageIdentifier]} alt={this.props.item.imageIdentifier}/>
+            <img src={Images[this.props.item.name]} alt={this.props.item.imageIdentifier}/>
           </div>
           <div className="click-item__upgrade-button">
             <span>Upgrade for {this.props.item.initialUpgradePrice} coins</span>
